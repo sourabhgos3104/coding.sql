66e1152ea73753d2dd858c3ee29d3a56de4e7f30
sourabhgos3104 <sourabhgoswmi3104@gmail.com>
2025-05-12 22:34:13 +0530

    Add files via upload

---
+++ sql class6.sql
@@ -0,0 +1,48 @@
+create database company;
+create table employee(empl_id int primary key,name varchar(30) not null,postiion varchar(30) not null,depatrment varchar(30) not null,salary int  
+,hiredate  date not null);
+
+insert into employee(empl_id,name,postiion,depatrment,salary,hiredate)values(1,"sourabh","datascience","ds",28000,"2025-05-12");
+
+select * from employee;
+
+
+rename table employee to employee1;
+select * from employee1;
+
+alter table employee1 add column country varchar(50) not null;
+alter table employee1 drop column country;
+
+update employee1 set country="India" where empl_id=1;
+
+alter table employee1 rename column  postiion to position;
+
+alter table employee1 rename column depatrment to department;
+
+
+
+insert into employee1(empl_id,name,position,department,salary,hiredate,country)values(2,"gorav","science","da",29000,"2024-04-12","usa");
+
+insert into employee1(empl_id,name,position,department,salary,hiredate,country)values(3,"ajay","science","da",29000,"2024-04-12","russia");
+
+select * from employee1 where salary=29000;
+describe employee1;
+
+select * from employee1 where country="india","usa";
+select * from employee1 where country in("india", "usa");
+
+select * from employee1 order by salary asc;
+select * from employee1 order by salary desc;
+select * from employee1 order by name ;
+
+#to create copy of structure
+create table employee15 as select * from employee1;
+select * from employee15;
+
+
+truncate table employee15;
+
+
+
+
+ 
